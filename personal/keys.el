47df624d05539b27c779ddeed8067b58c7a94dac
Aiden Scandella <aiden@uber.com>
2014-09-24 13:53:40 -0700

    initial customizations

---
+++ personal/keys.el
@@ -0,0 +1,19 @@
+(define-key evil-normal-state-map "s" 'evil-forward-char)
+
+(define-key evil-motion-state-map "n" 'evil-previous-line)
+(define-key evil-motion-state-map "t" 'evil-next-line)
+(define-key evil-motion-state-map "h" 'evil-backward-char)
+(define-key evil-motion-state-map "s" 'evil-forward-char)
+
+;; leader
+(require 'evil-leader)
+(global-evil-leader-mode)
+
+(evil-leader/set-leader ",")
+(evil-leader/set-key "e" 'find-file)
+(evil-leader/set-key "w" 'evil-write)
+(evil-leader/set-key "," 'evil-write)
+(evil-leader/set-key "." 'evil-write)
+
+(provide 'keys)
+;;; keys.el ends here
---
+++ personal/preload/evil-aiden.el
@@ -0,0 +1,5 @@
+;; enable control-u scroll up
+(setq evil-want-C-u-scroll t)
+
+(provide 'evil-aiden)
+;;; evil-aiden ends here
