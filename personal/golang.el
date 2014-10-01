e94cc23f25b5ae94f4574e4373cf57286b114552
Aiden Scandella <aiden@uber.com>
2014-09-30 18:37:21 -0700

    use prelude-go

--- personal/golang.el
+++ personal/golang.el
@@ -1,6 +1,4 @@
-;; gofmt before save
 (defun my-go-mode-hook ()
-  (add-hook 'before-save-hook 'gofmt-before-save)
   (setq tab-width 2 indent-tabs-mode 1))
 
 (add-hook 'go-mode-hook 'my-go-mode-hook)
