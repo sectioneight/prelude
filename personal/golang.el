bf73e5b29dfe092dad431a942c61c0e02d1f2060
Aiden Scandella <aiden@uber.com>
2015-04-28 09:00:02 -0700

    Add php mode for phabricator

--- personal/golang.el
+++ personal/golang.el
@@ -1,4 +1,5 @@
-;;; Code
+;;; Code:
+
 (defun my-go-mode-hook ()
   (whitespace-mode -1)
   (setq tab-width 2 indent-tabs-mode 1))
---
+++ personal/php.el
@@ -0,0 +1,9 @@
+;;; Code:
+
+(defun my-php-mode-hook ()
+  (shift-width 2)
+  (setq tab-width 2))
+
+(add-hook 'php-mode-hook 'my-php-mode-hook)
+
+;;; php.el ends here
