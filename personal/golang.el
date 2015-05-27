3f382397a7f529db5a21bbe8c23f1cbd6dd20ed8
Aiden Scandella <aiden@uber.com>
2015-05-27 14:35:56 -0700

    Fill column to 100 in Go

--- personal/golang.el
+++ personal/golang.el
@@ -2,7 +2,8 @@
 
 (defun my-go-mode-hook ()
   (whitespace-mode -1)
-  (setq tab-width 2 indent-tabs-mode 1))
+  (setq tab-width 2 indent-tabs-mode 1)
+  (setq fill-column 100))
 
 (add-hook 'before-save-hook #'gofmt-before-save)
 (add-hook 'go-mode-hook 'my-go-mode-hook)
